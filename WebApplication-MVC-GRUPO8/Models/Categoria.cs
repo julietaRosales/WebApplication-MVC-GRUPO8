a7d30992c7e4be92a71f96eb79fb0198f8883550
agustinresipa80 <42213711@comunidadort.edu.ar>
2025-11-05 20:16:14 -0300

    Se agerga nueva clase de categoria modelo

---
+++ WebApplication-MVC-GRUPO8/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC_GRUPO8.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public Boolean categoriaActivo { get; set; }
+        
+    }
+}
