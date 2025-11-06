77c9e24cf5c054e6096d90437e1154bd9175f08e
Juli Rosales <38292854@comunidadort.edu.ar>
2025-11-05 22:08:21 -0300

    se agrega clase comentario

---
+++ WebApplication-MVC-GRUPO8/Models/Comentario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC_GRUPO8.Models
+{
+    public class Comentario
+    {
+        public int id { get; set; }
+        public String texto { get; set; }
+        public int idUsuario { get; set; }
+
+        [Display(Name = "Fecha del comentario")]
+        public DateTime fechaComentario { get; set; }
+        public int idIncidencia { get; set; }
+    }
+}
