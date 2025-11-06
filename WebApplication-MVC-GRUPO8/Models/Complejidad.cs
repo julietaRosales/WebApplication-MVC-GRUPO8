958f0346d81ef9ab2115ae0283bc9585d75815b6
Juli Rosales <38292854@comunidadort.edu.ar>
2025-11-05 22:41:51 -0300

    se agrega enums

---
+++ WebApplication-MVC-GRUPO8/Models/Complejidad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_MVC_GRUPO8.Models
+{
+    public enum Complejidad
+    {
+        dificil,
+        intermedio,
+        facil
+    }
+}
