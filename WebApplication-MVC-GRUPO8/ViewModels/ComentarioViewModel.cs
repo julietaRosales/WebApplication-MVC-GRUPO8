b6d9f4aa385cdb407c899655a2ea4b3ed26cffe0
Juli Rosales <38292854@comunidadort.edu.ar>
2025-11-23 21:34:58 -0300

    se agrega validacion de comentario

--- WebApplication-MVC-GRUPO8/ViewModels/ComentarioViewModel.cs
+++ WebApplication-MVC-GRUPO8/ViewModels/ComentarioViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace WebApplication_MVC_GRUPO8.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC_GRUPO8.ViewModels
 {
     public class ComentarioViewModel
     {
+        [Required(ErrorMessage = "El comentario es obligatorio.")]
+        [StringLength(300, ErrorMessage = "El comentario no puede superar los 300 caracteres.")]
+        [Display(Name = "Comentario")]
         public String texto { get; set; }
         public int idIncidencia { get; set; }
 
