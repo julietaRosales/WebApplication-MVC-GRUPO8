1c8955905bf9b771aceef7f368f59b85524b66a6
agustinresipa80 <42213711@comunidadort.edu.ar>
2025-11-24 16:14:23 -0300

    Habilitar sesiones

--- WebApplication-MVC-GRUPO8/Program.cs
+++ WebApplication-MVC-GRUPO8/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddDbContext<SistemaMantenimientoDBContext>(options =>options.U
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Habilitar sesiones
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +29,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Activar sesión
+app.UseSession();
+
 
 app.UseAuthorization();
 
