90f8701d64880eef0a7bec27afcea7a547d55ffc
agustinresipa80 <42213711@comunidadort.edu.ar>
2025-11-24 16:13:26 -0300

    Modelo de login

---
+++ WebApplication-MVC-GRUPO8/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC_GRUPO8.ViewModels
+{
+    public class LoginViewModel
+    {
+        
+    [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    
+}
+}
