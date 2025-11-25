b6c330b0738ccda537a1f914e6d34a63175d824a
Juli Rosales <38292854@comunidadort.edu.ar>
2025-11-25 17:42:13 -0300

    se agrega validacion de inputs

--- WebApplication-MVC-GRUPO8/ViewModels/LoginViewModel.cs
+++ WebApplication-MVC-GRUPO8/ViewModels/LoginViewModel.cs
@@ -4,11 +4,13 @@ namespace WebApplication_MVC_GRUPO8.ViewModels
 {
     public class LoginViewModel
     {
-        
-    [Required]
+
+        [Required(ErrorMessage = "El emial es obligatorio")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El contraseña es obligatoria")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     
 }
